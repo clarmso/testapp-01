235baa88eb96e8f9bf50b4aecee228725b5da561
Clare So <cso@mozilla.com>
2023-08-03 02:13:16 -0400

    Day #3: Input param

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,23 @@
+import argparse
+
+def setUpParser():
+    parser = argparse.ArgumentParser(description='Process a message')
+    parser.add_argument('--message', '-m',
+                        type=int,
+                        help='an integer to determine the message')
+    return parser
+
+def determineMessage(message):
+    if message == 0:
+        return "I'm here."
+    elif message > 0:
+        return "Hello world!"
+    else:
+        return "Goodbye world!"
+
 def main():
-    print("Hello World!")
+    args = setUpParser().parse_args()
+    print(determineMessage(args.message))
 
 if __name__ == "__main__":
     main()
